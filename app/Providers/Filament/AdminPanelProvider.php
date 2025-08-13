60e4e26904cbba265b97f6d9c24d86825096a2ef
naghel <sia_k12000@yahoo.com>
2025-08-13 13:50:19 +0330

    feat(auth): install spatie/laravel-permission and setup roles & permissions

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            // ->registration()
+            // ->passwordReset()
+            // ->emailVerification()
             ->colors([
                 'primary' => Color::Amber,
             ])
